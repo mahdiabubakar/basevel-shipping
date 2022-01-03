ae60989c0d84c677138fd17c94843af297bad3b4
NigerianCoder <47560898+mahdiabubakar@users.noreply.github.com>
2022-01-03 11:34:53 +0100

    tailwindcss up and running

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3566;
 
 const httpServer = http.Server(app);
 const io = new Server(httpServer, { cors: { origin: '*' } });
