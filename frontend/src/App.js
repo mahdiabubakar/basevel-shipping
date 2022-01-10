de394b793da63dba7285443456301675b2954170
NigerianCoder <47560898+mahdiabubakar@users.noreply.github.com>
2022-01-10 03:02:46 +0100

    Navbar refactored

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { signout } from './actions/userActions';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import AdminRoute from './components/AdminRoute';
 import PrivateRoute from './components/PrivateRoute';
 import CartScreen from './screens/CartScreen';
@@ -22,11 +22,10 @@ import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import SellerRoute from './components/SellerRoute';
 import SellerScreen from './screens/SellerScreen';
-import SearchBox from './components/SearchBox';
+
 import SearchScreen from './screens/SearchScreen';
 import { listProductCategories } from './actions/productActions';
-import LoadingBox from './components/LoadingBox';
-import MessageBox from './components/MessageBox';
+
 import MapScreen from './screens/MapScreen';
 import DashboardScreen from './screens/DashboardScreen';
 import SupportScreen from './screens/SupportScreen';
@@ -34,143 +33,20 @@ import ChatBox from './components/ChatBox';
 
 // Styles
 import './App.css';
+import Navbar from './components/layouts/Navbar';
 
 function App() {
-  const cart = useSelector(state => state.cart);
-  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
-  const { cartItems } = cart;
   const userSignin = useSelector(state => state.userSignin);
   const { userInfo } = userSignin;
   const dispatch = useDispatch();
-  const signoutHandler = () => {
-    dispatch(signout());
-  };
 
-  const productCategoryList = useSelector(state => state.productCategoryList);
-  const {
-    loading: loadingCategories,
-    error: errorCategories,
-    categories,
-  } = productCategoryList;
   useEffect(() => {
     dispatch(listProductCategories());
   }, [dispatch]);
   return (
     <BrowserRouter>
       <div className='grid-container'>
-        <header className='row'>
-          <div>
-            <button
-              type='button'
-              className='open-sidebar'
-              onClick={() => setSidebarIsOpen(true)}>
-              <i className='fa fa-bars'></i>
-            </button>
-            <Link className='brand capitalize' to='/'>
-              amazona
-            </Link>
-          </div>
-          <div>
-            <SearchBox />
-          </div>
-          <div>
-            <Link to='/cart'>
-              Cart
-              {cartItems.length > 0 && (
-                <span className='badge'>{cartItems.length}</span>
-              )}
-            </Link>
-            {userInfo ? (
-              <div className='dropdown'>
-                <Link to='#'>
-                  {userInfo.name} <i className='fa fa-caret-down'></i>{' '}
-                </Link>
-                <ul className='dropdown-content'>
-                  <li>
-                    <Link to='/profile'>User Profile</Link>
-                  </li>
-                  <li>
-                    <Link to='/orderhistory'>Order History</Link>
-                  </li>
-                  <li>
-                    <Link to='#signout' onClick={signoutHandler}>
-                      Sign Out
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            ) : (
-              <Link to='/signin'>Sign In</Link>
-            )}
-            {userInfo && userInfo.isSeller && (
-              <div className='dropdown'>
-                <Link to='#admin'>
-                  Seller <i className='fa fa-caret-down'></i>
-                </Link>
-                <ul className='dropdown-content'>
-                  <li>
-                    <Link to='/productlist/seller'>Products</Link>
-                  </li>
-                  <li>
-                    <Link to='/orderlist/seller'>Orders</Link>
-                  </li>
-                </ul>
-              </div>
-            )}
-            {userInfo && userInfo.isAdmin && (
-              <div className='dropdown'>
-                <Link to='#admin'>
-                  Admin <i className='fa fa-caret-down'></i>
-                </Link>
-                <ul className='dropdown-content'>
-                  <li>
-                    <Link to='/dashboard'>Dashboard</Link>
-                  </li>
-                  <li>
-                    <Link to='/productlist'>Products</Link>
-                  </li>
-                  <li>
-                    <Link to='/orderlist'>Orders</Link>
-                  </li>
-                  <li>
-                    <Link to='/userlist'>Users</Link>
-                  </li>
-                  <li>
-                    <Link to='/support'>Support</Link>
-                  </li>
-                </ul>
-              </div>
-            )}
-          </div>
-        </header>
-        <aside className={sidebarIsOpen ? 'open' : ''}>
-          <ul className='categories'>
-            <li>
-              <strong>Categories</strong>
-              <button
-                onClick={() => setSidebarIsOpen(false)}
-                className='close-sidebar'
-                type='button'>
-                <i className='fa fa-close'></i>
-              </button>
-            </li>
-            {loadingCategories ? (
-              <LoadingBox></LoadingBox>
-            ) : errorCategories ? (
-              <MessageBox variant='danger'>{errorCategories}</MessageBox>
-            ) : (
-              categories.map(c => (
-                <li key={c}>
-                  <Link
-                    to={`/search/category/${c}`}
-                    onClick={() => setSidebarIsOpen(false)}>
-                    {c}
-                  </Link>
-                </li>
-              ))
-            )}
-          </ul>
-        </aside>
+        <Navbar />
         <main>
           <Routes>
             <Route path='/seller/:id' element={<SellerScreen />}></Route>
